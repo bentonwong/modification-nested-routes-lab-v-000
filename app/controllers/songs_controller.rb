04220126a6f6bda093d9ab6d2c96926ea7539d38
bentonwong <bentonwong@gmail.com>
2017-04-19 11:20:29 -0700

    add routes, modify controllers, views, helpers

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -24,13 +24,16 @@ class SongsController < ApplicationController
     end
   end
 
-  def new
-    @song = Song.new
+  def new # SongsController GET new sets artist when nested route
+    if params[:artist_id] && !Artist.exists?(params[:artist_id])
+      redirect_to artists_path, alert: "Artist not found."
+    else
+      @song = Song.new(artist_id: params[:artist_id])
+    end
   end
 
-  def create
+  def create # SongsController POST create accepts and sets artist_id
     @song = Song.new(song_params)
-
     if @song.save
       redirect_to @song
     else
@@ -38,15 +41,23 @@ class SongsController < ApplicationController
     end
   end
 
-  def edit
-    @song = Song.find(params[:id])
+  def edit # SongsController GET edit validates artist when nested
+    if params[:artist_id]
+      @artist = Artist.find_by(id: params[:artist_id])
+      if @artist.nil?
+        redirect_to artists_path, alert: "Artist not found."
+      else
+        @song = @artist.songs.find_by(id: params[:id])
+        redirect_to artist_songs_path(@artist), alert: "Song not found." if @song.nil?
+      end
+    else
+      @song = Song.find(params[:id])
+    end
   end
 
   def update
     @song = Song.find(params[:id])
-
     @song.update(song_params)
-
     if @song.save
       redirect_to @song
     else
@@ -64,7 +75,6 @@ class SongsController < ApplicationController
   private
 
   def song_params
-    params.require(:song).permit(:title, :artist_name)
+    params.require(:song).permit(:title, :artist_name, :artist_id)
   end
 end
-
